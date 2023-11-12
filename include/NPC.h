14e44c1768792d0a7b14b74e64fea92454560825
Michael Jones <mjj4@njit.edu>
2023-11-11 19:48:55 -0500

    forgot headerfile

---
+++ include/NPC.h
@@ -0,0 +1,40 @@
+#ifndef __NPC_H__
+#define __NPC_H__
+
+#include "entity.h"
+
+typedef enum{
+    NPC_dude = 0,
+    NPC_obj,
+    NPC_chest,
+    NPC_heal,
+    NPC_boost
+}NPC_Type;
+
+typedef struct {
+    NPC_Type type;
+    Sphere range;
+    char* message;
+    Uint8 talking;
+
+    //NPC_obj
+    Uint32 gold_c;
+
+    //NPC_obj
+    char* item_name;
+    //ITEM TYPE HERE
+
+    //NPC_heal
+    Uint32 heal_ammount;
+
+} NPC_data;
+
+/**
+ * @brief Create a new agumon entity
+ * @param position where to spawn the aguman at
+ * @return NULL on error, or an agumon entity pointer on success
+ */
+Entity* npc_new(Vector3D position);
+
+
+#endif
